06b22915a752dcc8e4ad3a13b1d2c8ee5bda6a62
Adam Koch <akoch@southsidechristian.org>
2019-09-26 13:56:13 -0400

    More updates.

---
+++ create_cluster.cmd
@@ -0,0 +1,8 @@
+cd %~dp0game
+kitchen create
+cd %~dp0micropayment
+kitchen create
+cd %~dp0web_frontend
+kitchen create
+cd %~dp0web_backend
+kitchen create
--- game/README.md
+++
@@ -1,3 +0,0 @@
-# learn_chef_iis
-
-This basic cookbook configures IIS on Windows Server 2012 R2.
